26d638c0c6e08c55d7562cca4285741bf83534f4
stevenmyers-wq <steven.myers@ringcentral.com>
2026-06-26 11:09:06 +1000

    Update utils.py

--- webapp/cost_centres/utils.py
+++ webapp/cost_centres/utils.py
@@ -21,19 +21,61 @@ def fetch_all_pages(endpoint, token, params=None):
     return records
 
 def get_cost_centres_data(token):
-    # 1. Fetch available cost centres - Safe fetch
+    # 1. Fetch available cost centres and build a lookup map
     try:
         cost_centres = fetch_all_pages('/restapi/v1.0/account/~/cost-center', token=token)
+        cc_map = {str(cc['id']): cc.get('name', 'Unknown') for cc in cost_centres}
     except Exception:
         cost_centres = []
-    
+        cc_map = {}
+
+    # 2. Fetch Account Default Cost Centre
+    account_default_cc_id = None
+    try:
+        acc_info = rc_api_call('/restapi/v1.0/account/~', method='GET', token=token)
+        if acc_info and acc_info.get('costCenter') and acc_info['costCenter'].get('id'):
+            account_default_cc_id = str(acc_info['costCenter']['id'])
+    except Exception:
+        pass
+
+    # 3. Fetch Sites and their associated Cost Centres
+    site_cc_map = {}
+    try:
+        sites = fetch_all_pages('/restapi/v1.0/account/~/sites', token=token)
+        for site in sites:
+            if site.get('costCenter') and site['costCenter'].get('id'):
+                site_cc_map[str(site['id'])] = str(site['costCenter']['id'])
+    except Exception:
+        pass
+
+    def resolve_cost_centre(item):
+        """Resolves the effective Cost Centre by checking Item -> Site -> Account inheritance"""
+        # A. Check explicit item assignment
+        cc_id = str(item.get('costCenter', {}).get('id', ''))
+        if cc_id:
+            return cc_id, cc_map.get(cc_id, 'Unknown')
+        
+        # B. Check site assignment
+        site_id = str(item.get('site', {}).get('id', ''))
+        if site_id and site_id in site_cc_map:
+            s_cc_id = site_cc_map[site_id]
+            return s_cc_id, cc_map.get(s_cc_id, 'Unknown')
+        
+        # C. Fallback to account default
+        if account_default_cc_id:
+            return account_default_cc_id, cc_map.get(account_default_cc_id, 'Unknown')
+            
+        return '', 'Unassigned'
+
     assets = []
     
-    # 2. Fetch Extensions (Users, IVRs, Queues, etc.)
+    # 4. Fetch Extensions (Users, IVRs, Queues, etc.)
     extensions = fetch_all_pages('/restapi/v1.0/account/~/extension', token=token)
     for ext in extensions:
         if ext.get('type') in ['Limited', 'ApplicationExtension']:
             continue
+            
+        cc_id, cc_name = resolve_cost_centre(ext)
             
         assets.append({
             'id': str(ext.get('id')),
@@ -43,15 +85,16 @@ def get_cost_centres_data(token):
             'number': ext.get('extensionNumber', 'N/A'),
             'site': ext.get('site', {}).get('name', 'Main Site'),
             'department': ext.get('contact', {}).get('department', 'N/A') or 'N/A',
-            'costCenterId': ext.get('costCenter', {}).get('id', ''),
-            'costCenterName': ext.get('costCenter', {}).get('name', 'Unassigned')
+            'costCenterId': cc_id,
+            'costCenterName': cc_name
         })
 
-    # 3. Fetch Phone Numbers
+    # 5. Fetch Phone Numbers
     phone_numbers = fetch_all_pages('/restapi/v1.0/account/~/phone-number', token=token)
     for pn in phone_numbers:
         usage = pn.get('usageType', '')
         if usage in ['CompanyNumber', 'MainCompanyNumber', 'DirectNumber'] and not pn.get('extension'):
+            cc_id, cc_name = resolve_cost_centre(pn)
             assets.append({
                 'id': str(pn.get('id')),
                 'type': 'PhoneNumber',
@@ -60,14 +103,15 @@ def get_cost_centres_data(token):
                 'number': pn.get('phoneNumber', ''),
                 'site': 'N/A',
                 'department': 'N/A',
-                'costCenterId': pn.get('costCenter', {}).get('id', ''),
-                'costCenterName': pn.get('costCenter', {}).get('name', 'Unassigned')
+                'costCenterId': cc_id,
+                'costCenterName': cc_name
             })
             
-    # 4. Fetch Devices
+    # 6. Fetch Devices
     devices = fetch_all_pages('/restapi/v1.0/account/~/device', token=token)
     for dev in devices:
         if not dev.get('extension'):
+            cc_id, cc_name = resolve_cost_centre(dev)
             assets.append({
                 'id': str(dev.get('id')),
                 'type': 'Device',
@@ -76,8 +120,8 @@ def get_cost_centres_data(token):
                 'number': dev.get('serial', 'N/A'),
                 'site': dev.get('site', {}).get('name', 'Main Site'),
                 'department': 'N/A',
-                'costCenterId': dev.get('costCenter', {}).get('id', ''),
-                'costCenterName': dev.get('costCenter', {}).get('name', 'Unassigned')
+                'costCenterId': cc_id,
+                'costCenterName': cc_name
             })
 
     return {'cost_centres': cost_centres, 'assets': assets}
